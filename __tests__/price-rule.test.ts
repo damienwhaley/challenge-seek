5a4a7da5baa16806c000c1e42b33d00f745a8570
Damien Whaley <damien@damienwhaley.com>
2021-04-20 08:26:29 +1000

    added first test for calculate method

--- __tests__/price-rule.test.ts
+++ __tests__/price-rule.test.ts
@@ -1,5 +1,7 @@
 import { PriceRule } from '../src/price-rule';
 import { Decimal } from 'decimal.js';
+import { ProductType } from '../src/product-type';
+import { Product } from '../src/product';
 
 describe('PriceRule class', () => {
   describe('::constructor()', () => {
@@ -8,6 +10,21 @@ describe('PriceRule class', () => {
 
       expect(typeof priceRule).not.toEqual('undefined');
       expect(priceRule instanceof PriceRule).toBe(true);
+    });
+  });
+
+  describe('::calculate()', () => {
+    it('can can calculate with a fixed discount', () => {
+      const productCodeFixture = ProductType.Classic;
+      const fixedDiscountFixture = new Decimal(10.0);
+      const retailPriceFixture = new Decimal(99.95);
+      const quantityFixture = 1;
+      const customerNameFixture = 'Lego';
+      const productFixture = new Product(productCodeFixture, retailPriceFixture);
+
+      const priceRule = new PriceRule(customerNameFixture, productCodeFixture, fixedDiscountFixture);
+
+      expect(priceRule.calculate(quantityFixture)).toEqual(retailPriceFixture - fixedDiscountFixture);
     });
   });
 });
