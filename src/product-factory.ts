14537b23283bb2d0438ea71ac1701e3a6da8f296
Damien Whaley <damien@damienwhaley.com>
2021-04-19 21:25:07 +1000

    satisfied test for initial create method

--- src/product-factory.ts
+++ src/product-factory.ts
@@ -1,3 +1,7 @@
-export class ProductFactory {
+import { Product } from './product';
 
+export class ProductFactory {
+  create(productCode: string): Product {
+    return new Product(productCode);
+  }
 };
