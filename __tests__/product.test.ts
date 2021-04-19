b86878977b6ec53328462e8f3abd058c580c4145
Damien Whaley <damien@damienwhaley.com>
2021-04-19 20:37:23 +1000

    about to add constructor options

--- __tests__/product.test.ts
+++ __tests__/product.test.ts
@@ -1,9 +1,15 @@
 import { Product } from '../src/product';
+import { Decimal } from 'decimal.js';
 
 describe('Product class', () => {
   describe('::constructor()', () => {
     it('can instatiate a class', () => {
-      const product = new Product();
+      const productCode = 'dummy';
+      const description = 'Some rather descriptive text';
+      const retailPrice = new Decimal(12.34);
+      const name = 'The Dummy Product';
+
+      const product = new Product(productCode, retailPrice, name, description);
 
       expect(typeof product !== 'undefined');
     });
