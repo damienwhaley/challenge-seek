5d908778ab8e1c21eae659bbe3b1ca260b16b2c9
Damien Whaley <damien@damienwhaley.com>
2021-04-20 08:09:17 +1000

    added code to satisfy customer name test, added test for product code

--- __tests__/advertisment.test.ts
+++ __tests__/advertisment.test.ts
@@ -3,7 +3,9 @@ import { Advertisment } from '../src/advertisment';
 describe('Advertisment class', () => {
   describe('::constructor()', () => {
     it('can instatiate a class', () => {
-      const advertisment = new Advertisment();
+      const customerNameFixture = 'Demo';
+      const productCodeFixture = 'basic';
+      const advertisment = new Advertisment(customerNameFixture, productCodeFixture);
 
       expect(typeof advertisment).not.toEqual('undefined');
       expect(advertisment instanceof Advertisment).toBe(true);
@@ -13,12 +15,24 @@ describe('Advertisment class', () => {
   describe('::getCustomerName', () => {
     it('can get a customer name', () => {
       const customerNameFixture = 'Example';
-      const productCodeFixture = 'example';
+      const productCodeFixture = 'simple';
       const advertisment = new Advertisment(customerNameFixture, productCodeFixture);
 
       const result = advertisment.getCustomerName();
 
       expect(result).toEqual(customerNameFixture);
+    });
+  });
+
+  describe('::getProductCode', () => {
+    it('can get a product code', () => {
+      const customerNameFixture = 'Amazing';
+      const productCodeFixture = 'advanced';
+      const advertisment = new Advertisment(customerNameFixture, productCodeFixture);
+
+      const result = advertisment.getProductCode();
+
+      expect(result).toEqual(productCodeFixture);
     });
   });
 });
--- src/advertisment.ts
+++ src/advertisment.ts
@@ -1,3 +1,13 @@
 export class Advertisment {
+  private customerName: string;
+  private productCode: string;
 
+  constructor(customerName: string, productCode: string) {
+    this.customerName = customerName;
+    this.productCode = productCode;
+  }
+
+  getCustomerName(): string {
+    return this.customerName;
+  }
 };
