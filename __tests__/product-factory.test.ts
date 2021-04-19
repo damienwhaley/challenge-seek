28cd75b18e350b7683a01a614bd7f48041ed605f
Damien Whaley <damien@damienwhaley.com>
2021-04-19 21:22:55 +1000

    adding tests for create

--- __tests__/product-factory.test.ts
+++ __tests__/product-factory.test.ts
@@ -1,4 +1,6 @@
 import { ProductFactory } from '../src/product-factory';
+import { ProductType } from '../src/product-type';
+import { Product } from '../src/product';
 
 describe('ProductFactory class', () => {
   describe('::constructor()', () => {
@@ -6,6 +8,17 @@ describe('ProductFactory class', () => {
       const productFactory = new ProductFactory();
 
       expect(typeof productFactory).not.toEqual('undefined');
+    });
+  });
+
+  describe('::create()', () => {
+    it('can create a Classic Ad product', () => {
+      const productCodeFixture = ProductType.Classic;
+
+      const productFactory = new ProductFactory();
+      const result = productFactory.create(productCodeFixture);
+      expect(typeof result).not.toEqual('undefined');
+      expect(result instanceof Product).toBe(true);
     });
   });
 });
