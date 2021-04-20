708c226cf6023637bc829067d4e9cccf19f92f0b
Damien Whaley <damien@damienwhaley.com>
2021-04-20 22:19:17 +1000

    tests and code for adding advertisments

--- __tests__/checkout.test.ts
+++ __tests__/checkout.test.ts
@@ -1,12 +1,86 @@
 import { Checkout } from '../src/checkout';
+import { PricingRules } from  '../src/pricing-rules';
+import { Advertisment } from '../src/advertisment';
 
 describe('Checkout class', () => {
   describe('::constructor()', () => {
     it('can instatiate a class', () => {
-      const checkout = new Checkout();
+      const pricingRulesFixture = new PricingRules();
+      const checkout = new Checkout(pricingRulesFixture);
 
       expect(typeof checkout).not.toEqual('undefined');
       expect(checkout instanceof Checkout).toBe(true);
+    });
+  });
+
+  describe('::add()', () => {
+    let pricingRulesFixture: PricingRules;
+    let checkout: Checkout;
+
+    beforeEach(() => {
+      pricingRulesFixture = new PricingRules();
+      checkout = new Checkout(pricingRulesFixture);
+    });
+
+    it('can add an advertisment', () => {
+      const customerNameFixture = 'Dana Scully';
+      const productCodeFixture = 'X-Files';
+      const advertismentFixture = new Advertisment(customerNameFixture, productCodeFixture);
+
+      checkout.add(advertismentFixture);
+
+      expect(checkout.count()).toEqual(1);
+    });
+
+    it('can add the same advertisment multiple times', () => {
+      const customerNameFixture = 'Fox Mulder';
+      const productCodeFixture = 'X-Files';
+      const advertismentFixture = new Advertisment(customerNameFixture, productCodeFixture);
+
+      checkout.add(advertismentFixture);
+      checkout.add(advertismentFixture);
+      checkout.add(advertismentFixture);
+
+      expect(checkout.count()).toEqual(3);
+    });
+
+    it('can add different advertisments', () => {
+      const customerNameFixture1 = 'Smoking Man';
+      const productCodeFixture1 = 'X-Files';
+      const advertismentFixture1 = new Advertisment(customerNameFixture1, productCodeFixture1);
+
+      const customerNameFixture2 = 'Walter Skinner';
+      const productCodeFixture2 = 'X-Files';
+      const advertismentFixture2 = new Advertisment(customerNameFixture2, productCodeFixture2);
+
+      checkout.add(advertismentFixture1);
+      checkout.add(advertismentFixture2);
+
+      expect(checkout.count()).toEqual(2);
+    });
+
+    it('can add different advertisments in varying quantities', () => {
+      const customerNameFixture1 = 'Chris Carter';
+      const productCodeFixture1 = 'X-Files';
+      const advertismentFixture1 = new Advertisment(customerNameFixture1, productCodeFixture1);
+
+      const customerNameFixture2 = 'Mark Snow';
+      const productCodeFixture2 = 'X-Files';
+      const advertismentFixture2 = new Advertisment(customerNameFixture2, productCodeFixture2);
+
+      const customerNameFixture3 = 'Trust';
+      const productCodeFixture3 = 'No1';
+      const advertismentFixture3 = new Advertisment(customerNameFixture3, productCodeFixture3);
+
+      checkout.add(advertismentFixture1);
+      checkout.add(advertismentFixture2);
+      checkout.add(advertismentFixture3);
+      checkout.add(advertismentFixture1);
+      checkout.add(advertismentFixture2);
+      checkout.add(advertismentFixture2);
+      checkout.add(advertismentFixture3);
+
+      expect(checkout.count()).toEqual(7);
     });
   });
 });
--- src/checkout.ts
+++ src/checkout.ts
@@ -1,3 +1,45 @@
-export class Checkout {
+import { PricingRules } from './pricing-rules';
+import { Advertisment } from './advertisment';
 
+interface AdvertismentItem {
+  customerName: string,
+  productCode: string,
+  quantity: number
+};
+
+export class Checkout {
+  private pricingRules: PricingRules;
+  private advertisments: { key: string, value: AdvertismentItem };
+
+  private createKey(customerName: string, productCode: string): string {
+    return `${customerName}(╯°□°)╯︵ ┻━┻${productCode}`;
+  }
+
+  constructor(pricingRules: PricingRules) {
+    this.pricingRules = pricingRules;
+  }
+
+  add(advertisment: Advertisment) {
+    const key = this.createKey(advertisment.getCustomerName(), advertisment.getProductCode());
+
+    if (key in this.advertisments) {
+      this.advertisments[key].quantity = this.advertisments[key].quantity + 1;
+    } else {
+      this.advertisments[key] = {
+        customerName: advertisment.getCustomerName(),
+        productCode: advertisment.getProductCode(),
+        quantity: 1
+      };
+    }
+  }
+
+  count(): number {
+    let quantity = 0;
+
+    Object.keys(this.advertisments).forEach((key) => {
+      quantity = quantity + this.advertisments[key].quantity;
+    });
+
+    return quantity;
+  }
 };
